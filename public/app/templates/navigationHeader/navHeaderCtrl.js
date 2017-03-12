b15a7bfe3ff4d17ca49e66a8157e89ab94a536ec
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2017-03-12 00:51:03 -0800

    navigation

--- public/app/templates/navigationHeader/navHeaderCtrl.js
+++ public/app/templates/navigationHeader/navHeaderCtrl.js
@@ -1,8 +1,18 @@
-var navHeaderTmpFn = function($scope, CONSTANTS) {
-    function init() {
-        $scope.headerTemplate = CONSTANTS.HEADER_TEMPLATE;
-    }
-    init();
+var navHeaderTmpFn = function ($scope, CONSTANTS, $location, $timeout) {
+	function init() {
+		$scope.headerTemplate = CONSTANTS.HEADER_TEMPLATE;
+	}
+	init();
+	var pathName = $location.url();
+
+	$scope.$on('$routeChangeStart', function (next, current) {
+		pathName = $location.url();
+		$timeout(function () {
+			angular.element("nav .navbar-nav li").removeClass("active");
+			angular.element("nav li a[href='#" + pathName + "']").closest("li").addClass("active");
+
+		}, 100);
+	});
 };
 
-navModule.controller('headerNavCtrl', ['$scope', 'APP_CONSTANTS', navHeaderTmpFn]);
+navModule.controller('headerNavCtrl', ['$scope', 'APP_CONSTANTS', '$location', '$timeout', navHeaderTmpFn]);
