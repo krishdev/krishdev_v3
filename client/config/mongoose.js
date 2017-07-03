00c869c870dad53e1477fa100bbf5600e3dba1bf
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2017-07-03 14:48:49 -0700

    mongoose openuri

--- client/config/mongoose.js
+++ client/config/mongoose.js
@@ -2,5 +2,7 @@ var mongoose = require('mongoose');
 var config = require('./config');;
 module.exports = function () {
 	mongoose.Promise = global.Promise;
-	mongoose.connect(config.dbconnection);
+	mongoose.connect(config.dbconnection, {
+		useMongoClient: true
+	});
 };
