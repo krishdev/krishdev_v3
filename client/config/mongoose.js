d56253ff4b4174f18d0b64986d6d02d646802a28
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2017-07-02 21:15:53 -0700

    added promise in mongoose

--- client/config/mongoose.js
+++ client/config/mongoose.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var config = require('./config');;
 module.exports = function () {
+	mongoose.Promise = global.Promise;
 	mongoose.connect(config.dbconnection);
 };
