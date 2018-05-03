4563d09f5eb6f3f6eb73e16331b3e421475fbce2
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2018-05-02 22:16:06 -0500

    updated cors

--- client/config/express.js
+++ client/config/express.js
@@ -31,7 +31,11 @@ module.exports = function () {
 	app.set("view engine", "ejs");
 	app.use(express.static("./public"));
 	console.log("express initialized");
-
+	app.all('/*', function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	  next();
+	});
 	require("../routers/router.main")(app);
 
 	return app;
